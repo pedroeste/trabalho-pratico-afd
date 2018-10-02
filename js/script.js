286802b8cfb8ebefbe9922fe5c2df25e2fedfd22
Pedro Fernandez <pedro.estefernandez@yahoo.com.br>
2018-10-02 14:02:48 -0300

    melhorando a maquina de casos

--- js/script.js
+++ js/script.js
@@ -2,63 +2,110 @@ let input = document.querySelector('#afd');
 let aceito = document.querySelector('.aceita');
 let naoAceito = document.querySelector('.nao-aceita');
 
+let pComprimento = document.querySelector('.comprimento');
+let pTexto = document.querySelector('.texto');
+let pUltimoChar = document.querySelector('.ultimo-char');
+let pKeycodeChar = document.querySelector('.keycode-char');
+let pCharIgual = document.querySelector('.char-igual');
+
 let caseValue;
+let attrSymbol = false;
+
+let firstChar = /[a-z]/;
 
 function checkString() {
-    var inputValue = this.value;
-    var lastChar = inputValue[inputValue.length-1];
+	var inputValue = this.value;
+	var lastChar = inputValue[inputValue.length-1];
 
-    console.log(`String do input: ${inputValue}`);
-    console.log(`Comprimento da string: ${inputValue.length}`);
+	//PRINT NA TELA
+	pTexto.innerHTML = `String do input: ${inputValue}`;
+	pComprimento.innerHTML = `Comprimento da string: ${inputValue.length}`;	
 
-    function checkFirstLetter(){
+	
+	if(inputValue.length <= 1){
+		
+		console.log(firstChar.test(inputValue));
 
-    }
+		if(firstChar.test(inputValue)){
+			caseValue = 'pass';
 
-    
-    if(inputValue.length <= 1){
-        var firstString = /[a-zA-Z]/;
-        
-        console.log(firstString.test(inputValue));
+		} else {
+			caseValue = 'lixo';
 
-        if(firstString.test(inputValue)){
-            caseValue = 2;
-        } else {
-            caseValue = 'lixo';
-        }
+		}
 
-    } else if(inputValue.length >= 2){
-        var identifier = /[0-9a-zA-Z|_|=]/;
+	} else if(lastChar != '=' && !attrSymbol){
+		var identifier = /[0-9a-zA-Z|_|\s]/;
 
-        console.log(`Último char digitado: ${lastChar}`);
-        console.log(`KeyCode do char: ${lastChar.charCodeAt(0)}`)
+		// PRINT NA TELA
+		pUltimoChar.innerHTML = `Último char digitado: ${lastChar}`;
+		pKeycodeChar.innerHTML = `KeyCode do char: ${lastChar.charCodeAt(0)}`;
+		console.log(identifier.test(lastChar));
 
-        console.log(identifier.test(lastChar));
+		if(!identifier.test(lastChar)){
+			caseValue = 'lixo';
+		}
 
-        if(!identifier.test(lastChar)){
-            caseValue = 'lixo';
+		// se o ultimo caracter digitado for um '=' e não tiver nenhum '=' na string:
+	} else if(lastChar == '='){
+		if(!attrSymbol){
+			// PRINT NA TELA
+			pCharIgual.innerHTML = `String é um = e não existe = na string'`;
+			attrSymbol = true;
+		} else {
+			pCharIgual.innerHTML = `String é um = e já existe = na string`;
+			caseValue = 'lixo';
+		}
+		
+		// Se ja houver pelo menos um '=' no código e o ultimo caracter digitado for diferente de '=':
+	} else if(lastChar != '=' && attrSymbol) {
+		pCharIgual.innerHTML = `String diferente de = e já existe um =`;
+		if(firstChar.test(inputValue)){
+			caseValue = 'pass';
+			if(lastChar == ';'){
+				caseValue = 'aceito';
+			}
 
-        } else if(lastChar.charCodeAt(0) == 61) {
-            
-        }
+		} else {
+			caseValue = 'lixo';
 
-    }
+		}
+	}
 
-    switch(caseValue) {
-        case 2:
-            break;
 
-        case 'aceito':
-            aceito.style.display = 'block';
-            break;
-        
-        case 'lixo':
-            naoAceito.style.display = 'block';
-            input.disabled = true;
-            break;
-    }
 
-    console.log('');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	switch(caseValue) {
+		case 'pass':
+			break;
+
+		case 'aceito':
+			aceito.style.display = 'block';
+			input.disabled = true;
+			break;
+		
+		case 'lixo':
+			naoAceito.style.display = 'block';
+			input.disabled = true;
+			break;
+	}
+
+	console.log('');
 }
 
 input.addEventListener('keyup', checkString);
