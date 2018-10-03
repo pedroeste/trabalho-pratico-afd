89daf1f9d76c371cba6097274b1f6b071897eeb1
Pedro Fernandez <pedro.estefernandez@yahoo.com.br>
2018-10-02 23:04:11 -0300

    abordagem pós criação do autômato

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,5 @@
-let simpleAttrRegex = /[a-z]([0-9a-zA-Z])*(\s)*=(\s)*([a-z]([0-9a-zA-Z])*)[0-9]*;/g;
+let identificador = /[a-z]([a-z]|[0-9]|[_])*/g;
+let numero = /([0-9]([0-9])*)|[0-9]([0-9]([0-9])*'.'[0-9]([0-9])*)]/g;
 
-let fullAttrRegex = /[a-z]([0-9a-zA-Z])*(\s)*=(\s)*([a-z]([0-9a-zA-Z])*)[0-9]*(\s)*[+](\s)*[^$]((([a-z]([0-9a-zA-Z])*)*)+[0-9]);/g;
+// nem todas as condições funcionam
+let operadores = /[(\+|-|\*|\/)]/g;
