65e2ef48d355fb7dd07b43a0c44a38ebbedb31d5
Pedro Fernandez <pedro.estefernandez@yahoo.com.br>
2018-11-11 19:38:05 -0200

    automato de for

--- js/script3.js
+++ js/script3.js
@@ -1,111 +1,72 @@
-let input = document.querySelector('#afd');
-let aceito = document.querySelector('.aceita');
-let naoAceito = document.querySelector('.nao-aceita');
+let caseValue = 'for';
+let numCounter = false;
+let forCounter = 0;
 
-let pComprimento = document.querySelector('.comprimento');
-let pTexto = document.querySelector('.texto');
-let pUltimoChar = document.querySelector('.ultimo-char');
-let pKeycodeChar = document.querySelector('.keycode-char');
-let pCharIgual = document.querySelector('.char-igual');
-
-let caseValue;
-let attrSymbol = false;
-
-let firstChar = /[a-z]/;
-
-function checkString() {
-	var inputValue = this.value;
-	var lastChar = inputValue[inputValue.length-1];
-
-	//PRINT NA TELA
-	pTexto.innerHTML = `String do input: ${inputValue}`;
-	pComprimento.innerHTML = `Comprimento da string: ${inputValue.length}`;	
-
-	
-	if(inputValue.length <= 1){
-		
-		console.log(firstChar.test(inputValue));
-
-		if(firstChar.test(inputValue)){
-			caseValue = 'pass';
-
-		} else {
-			caseValue = 'lixo';
-
-		}
-
-	} else if(lastChar != '=' && !attrSymbol){
-		var identifier = /[0-9a-zA-Z|_|\s]/;
-
-		// PRINT NA TELA
-		pUltimoChar.innerHTML = `Último char digitado: ${lastChar}`;
-		pKeycodeChar.innerHTML = `KeyCode do char: ${lastChar.charCodeAt(0)}`;
-		console.log(identifier.test(lastChar));
-
-		if(!identifier.test(lastChar)){
-			caseValue = 'lixo';
-		}
-
-		// se o ultimo caracter digitado for um '=' e não tiver nenhum '=' na string:
-	} else if(lastChar == '='){
-		if(!attrSymbol){
-			// PRINT NA TELA
-			pCharIgual.innerHTML = `String é um = e não existe = na string'`;
-			attrSymbol = true;
-		} else {
-			pCharIgual.innerHTML = `String é um = e já existe = na string`;
-			caseValue = 'lixo';
-		}
-		
-		// Se ja houver pelo menos um '=' no código e o ultimo caracter digitado for diferente de '=':
-	} else if(lastChar != '=' && attrSymbol) {
-		pCharIgual.innerHTML = `String diferente de = e já existe um =`;
-		if(firstChar.test(inputValue)){
-			caseValue = 'pass';
-			if(lastChar == ';'){
-				caseValue = 'aceito';
-			}
-
-		} else {
-			caseValue = 'lixo';
-
-		}
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	switch(caseValue) {
-		case 'pass':
-			break;
-
-		case 'aceito':
-			aceito.style.display = 'block';
-			input.disabled = true;
-			break;
-		
-		case 'lixo':
-			naoAceito.style.display = 'block';
-			input.disabled = true;
-			break;
-	}
-
-	console.log('');
+function lixo(){
+    naoAceito.style.display = 'block';
+    input.disabled = true;
 }
 
-input.addEventListener('keyup', checkString);
+function aceito(){
+    divAceito.style.display = 'block';
+    input.disabled = true;
+}
+
+function testString(){
+	var inputValue = this.value;
+	var lastChar = inputValue[inputValue.length-1];
+	
+	switch(caseValue){
+		case 'for':
+			if(lastChar === 'f' && forCounter == 0){
+				forCounter = 1;
+
+			} else if(lastChar === 'o' && forCounter == 1){
+				forCounter = 2;
+
+			} else if(lastChar === 'r' && forCounter == 2) {
+				caseValue = 'parametro1';
+
+			} else {
+                lixo();
+
+            }
+			break;
+		case 'parametro1':
+			if(lastChar === '('){
+				caseValue = 'char';
+
+			} else {
+				lixo();
+
+			}
+			break;
+		case 'char':
+			if(/[a-z]/.test(lastChar)){
+				caseValue = 'identificador';
+
+			} else {
+				lixo();
+
+			}
+			break;
+		case 'identificador':
+			if(/([a-z]|[0-9]|[_])/.test(lastChar)){
+				caseValue = 'identificador';
+
+			} else if(lastChar == '='){
+				caseValue = 'num';
+
+			} else {
+				lixo();
+			}
+			break;
+		case 'num':
+			if(/[0-9]/.test(lastChar)){
+				
+			}
+			break;
+	}
+}
+
+input.addEventListener('keyup', testString);
